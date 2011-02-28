79a01b5492f280cc8c9ceebeef046e085e684f48
Guillaume Desmottes <guillaume.desmottes@collabora.co.uk>
2011-02-28 13:13:33 +0100

    empathy-accounts: allow to pass the full path of the account

--- src/empathy-accounts.c
+++ src/empathy-accounts.c
@@ -112,8 +112,12 @@ account_manager_ready_for_accounts_cb (GObject *source_object,
 
       /* create and prep the corresponding TpAccount so it's fully ready by the
        * time we try to select it in the accounts dialog */
-      account_path = g_strdup_printf ("%s%s", TP_ACCOUNT_OBJECT_PATH_BASE,
-          selected_account_name);
+      if (g_str_has_prefix (selected_account_name, TP_ACCOUNT_OBJECT_PATH_BASE))
+        account_path = g_strdup (selected_account_name);
+      else
+        account_path = g_strdup_printf ("%s%s", TP_ACCOUNT_OBJECT_PATH_BASE,
+            selected_account_name);
+
       bus = tp_dbus_daemon_dup (NULL);
       if ((account = tp_account_new (bus, account_path, &error)))
         {
