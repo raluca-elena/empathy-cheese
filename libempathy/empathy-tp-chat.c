7412b7e93ee7106dc139365fb391e2f613c294ef
Jonny Lamb <jonnylamb@gnome.org>
2011-05-12 16:24:43 +0100

    tp-chat: add pending-message-removed signal
    Signed-off-by: Jonny Lamb <jonnylamb@gnome.org>

--- libempathy/empathy-tp-chat.c
+++ libempathy/empathy-tp-chat.c
@@ -84,6 +84,7 @@ enum {
 	CHAT_STATE_CHANGED,
 	PROPERTY_CHANGED,
 	DESTROY,
+	PENDING_MESSAGE_REMOVED,
 	LAST_SIGNAL
 };
 
@@ -480,6 +481,8 @@ pending_message_removed_cb (TpTextChannel   *channel,
 
 	g_object_unref (m->data);
 	g_queue_delete_link (priv->pending_messages_queue, m);
+
+	g_signal_emit (chat, signals[PENDING_MESSAGE_REMOVED], 0);
 }
 
 static void
@@ -1655,6 +1658,16 @@ empathy_tp_chat_class_init (EmpathyTpChatClass *klass)
 
 	signals[DESTROY] =
 		g_signal_new ("destroy",
+			      G_TYPE_FROM_CLASS (klass),
+			      G_SIGNAL_RUN_LAST,
+			      0,
+			      NULL, NULL,
+			      g_cclosure_marshal_VOID__VOID,
+			      G_TYPE_NONE,
+			      0);
+
+	signals[PENDING_MESSAGE_REMOVED] =
+		g_signal_new ("pending-message-removed",
 			      G_TYPE_FROM_CLASS (klass),
 			      G_SIGNAL_RUN_LAST,
 			      0,
