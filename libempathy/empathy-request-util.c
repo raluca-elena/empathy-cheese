4644a9d69f070df504925902e7692fa1bc731dfa
Guillaume Desmottes <guillaume.desmottes@collabora.co.uk>
2011-06-14 10:12:45 +0100

    Set the DelegateToPreferredHandler hint
    This ensures that Empathy will always handle and display the channel, even if
    GNOME Shel is already handling it.
    
    https://bugzilla.gnome.org/show_bug.cgi?id=652439

--- libempathy/empathy-request-util.c
+++ libempathy/empathy-request-util.c
@@ -80,6 +80,7 @@ create_text_channel (TpAccount *account,
         TP_PROP_CHANNEL_INTERFACE_SMS_SMS_CHANNEL, TRUE);
 
   req = tp_account_channel_request_new (account, request, timestamp);
+  tp_account_channel_request_set_delegate_to_preferred_handler (req, TRUE);
 
   tp_account_channel_request_ensure_channel_async (req, EMPATHY_CHAT_BUS_NAME,
       NULL, ensure_text_channel_cb, NULL);
